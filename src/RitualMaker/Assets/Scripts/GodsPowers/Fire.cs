b850a72a10b781d15aea2954fa281b4c36ba9fe4
Anton Monjon <monjon.an@gmail.com>
2016-01-30 14:40:56 +0100

    fixing assets file size issue

---
+++ src/RitualMaker/Assets/Scripts/GodsPowers/Fire.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	public GameController gc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void PowerActivated(){
+		Debug.Log ("Change mouse cursor");
+		gc.ActivatePower ("Fire");
+	}
+}
