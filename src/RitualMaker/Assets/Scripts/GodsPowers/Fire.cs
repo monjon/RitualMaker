aed0f3fefe3dc6fae0cddd21902f17298edd00b0
Anton Monjon <monjon.an@gmail.com>
2016-01-30 16:39:15 +0100

    Finalize fire fx particle

--- src/RitualMaker/Assets/Scripts/GodsPowers/Fire.cs
+++ src/RitualMaker/Assets/Scripts/GodsPowers/Fire.cs
@@ -5,29 +5,15 @@ public class Fire : MonoBehaviour {
 
 	public GameController gc;
 
-	private float duration = 1.0f;
-	private float alpha = 0f;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	public void PowerActivated(){
 		Debug.Log ("Change mouse cursor");
 		gc.ActivatePower ("Fire");
 	}
-
-	void Update(){
-
-		lerpAlpha();
-	}
-
-	void lerpAlpha () {
-
-		float lerp = Mathf.PingPong (Time.time, duration) / duration;
-
-		alpha = Mathf.Lerp(0.0f, 1.0f, lerp) ;
-		GetComponent<Renderer>().material.color.a = alpha;
-	}
+		
 }
