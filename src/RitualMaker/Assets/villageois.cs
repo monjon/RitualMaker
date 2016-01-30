63f7a77baacb2b4569b2fb3547fee86eef0a79d2
gilles merletti <gilles.merletti@epitech.eu>
2016-01-30 15:27:25 +0100

    itineraire prédefinis complex pour les villageois

--- src/RitualMaker/Assets/villageois.cs
+++ src/RitualMaker/Assets/villageois.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class villageois : MonoBehaviour {
     private Vector3 destination;
-    public Vector3 village;
-    public Vector3 workplace;
+    public List<Vector3> villageToWorkplace;
 
     public float speed;
+    public float collectTime = 10f;
 
-    private int maxFood;
+    public int maxFood = 10;
     public int food = 0;
     public string sex;
     public string job;
     public string health;
     public string age;
     private float timer = 0;
+    private int i = 0;
 
     private enum playerState
     {
@@ -33,7 +35,7 @@ public class villageois : MonoBehaviour {
     void moove()
     {
         if (transform.position != destination)
-            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
     }
 	
 	// Update is called once per frame
@@ -41,28 +43,35 @@ public class villageois : MonoBehaviour {
         switch (pState)
         {
             case playerState.isGoingToWork:
-                destination = workplace;
+                destination = villageToWorkplace[i];
                 moove();
-                if (transform.position == workplace)
+                if (transform.position == villageToWorkplace[villageToWorkplace.Count -1])
+                {
                     pState = playerState.isWorking;
+                }
+                else if (transform.position == villageToWorkplace[i])
+                    ++i;
                 break;
 
             case playerState.isGoingBackHome:
-                destination = village;
+                destination = villageToWorkplace[i];
                 moove();
-                if (transform.position == village)
+                if (transform.position == villageToWorkplace[0])
                 {
                     pState = playerState.isGoingToWork;
                     food = 0;
                 }
+                else if (transform.position == villageToWorkplace[i])
+                    --i;
                 break;
 
             case playerState.isWorking:
                 timer = timer + Time.deltaTime;
-                if (food < maxFood && timer >= 10f)
+                if (food < maxFood && timer >= collectTime)
                 {
                     timer = 0f;
                     food++;
+                    Debug.Log(food);
                 }
                 else if (food == maxFood)
                 {
