876a0dca811ec681dabc512b93383e5125eaf338
scrubz <thomas.epitech@gmail.com>
2016-01-30 14:17:11 +0100

    Adding Keywords, Ritual, and ritual manager / prefabs.

---
+++ src/RitualMaker/Assets/Scripts/KeyWords.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyWords : MonoBehaviour 
+{
+
+    [HideInInspector]
+    public List<string> KeyWordsList;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
---
+++ src/RitualMaker/Assets/Scripts/Ritual.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ritual : MonoBehaviour 
+{
+
+    [HideInInspector]
+    public List<string> keywords;
+    [HideInInspector]
+    public string godAction;
+    [HideInInspector]
+    public int faith;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
