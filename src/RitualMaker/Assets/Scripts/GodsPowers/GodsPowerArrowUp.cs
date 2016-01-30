6396e0f3926f57577e5b0f2712112bdb1d103be1
Anton Monjon <monjon.an@gmail.com>
2016-01-30 20:19:49 +0100

    working in progress commit

---
+++ src/RitualMaker/Assets/Scripts/GodsPowers/GodsPowerArrowUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GodsPowerArrowUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
