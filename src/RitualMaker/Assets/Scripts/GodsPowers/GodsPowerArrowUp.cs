cdfbb2983c8411684693e0a11ca9898792471319
Anton Monjon <monjon.an@gmail.com>
2016-01-30 21:24:28 +0100

    work in progress files committed

--- src/RitualMaker/Assets/Scripts/GodsPowers/GodsPowerArrowUp.cs
+++ src/RitualMaker/Assets/Scripts/GodsPowers/GodsPowerArrowUp.cs
@@ -6,9 +6,13 @@ public class GodsPowerArrowUp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Vector3 newPos = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.y);
-		transform.DOMove(newPos, 1);
-		transform.DOFade (1, 1);
+		Vector3 newPos = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.y);
+		transform.DOMove (newPos, 0.5f).OnComplete (()=>ChangeAlpha()).SetDelay(0.5f);
+	}
+
+	void ChangeAlpha(){
+
+		DOTween.ToAlpha(()=> gameObject.GetComponent<SpriteRenderer>().color, x => gameObject.GetComponent<SpriteRenderer>().color = x, 0, 0.5f);
 	}
 	
 	// Update is called once per frame
