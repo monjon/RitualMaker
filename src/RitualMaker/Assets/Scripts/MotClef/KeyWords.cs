1b4df954d3c2b0966c24e84c1a6b8d90588b3977
scrubz <thomas.epitech@gmail.com>
2016-01-30 11:12:28 +0100

    Adding keywords and ritual. Ritual has a prefab.

---
+++ src/RitualMaker/Assets/Scripts/MotClef/KeyWords.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyWords : MonoBehaviour 
+{
+    public List<string> KeyWordsList = new List<string>();
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
