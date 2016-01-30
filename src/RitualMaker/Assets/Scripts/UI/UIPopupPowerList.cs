92353a947fc1cf9a4e065b0c482a8ec8cd82a30d
Renaud THIERRY <thierry.renaud.laurent@gmail.com>
2016-01-30 11:27:36 +0100

    Added : UIScreenInGame Added : UIPopUpPrefab Added : UIScreenTitle

---
+++ src/RitualMaker/Assets/Scripts/UI/UIPopupPowerList.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIPopupPowerList : UIPopUp {
+
+	// Properties
+	// 
+
+	public RectTransform PowerWheelBase;
+
+	// Methods
+	//
+
+	override
+	public void OpenPopUp(){
+		base.OpenPopUp();
+
+		if(this.PowerWheelBase != null){
+
+			Vector3 pos = new Vector3(Mathf.Clamp(Input.mousePosition.x, Camera.main.pixelWidth *0.15f, Camera.main.pixelWidth*0.85f), Mathf.Clamp(Input.mousePosition.y,Camera.main.pixelHeight *0.15f, Camera.main.pixelHeight*0.85f ), 0f);
+
+			this.PowerWheelBase.anchoredPosition3D = pos;
+
+			// Debug.Log("UIPopupPowerList.OpenPopUp - pos "+pos+", InputMousePos "+Input.mousePosition);
+
+		}
+
+	}
+
+}
---
+++ src/RitualMaker/Assets/Scripts/UI/UIScreenInGame.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScreenInGame : UIScreen {
+
+	// Properties
+	//
+
+	public bool isCastingPower = false;
+
+	//TODO :
+	//public Power CurrentSelectedPower;
+
+	// Methods
+	//
+
+	public void Update(){
+
+		// On a left click
+		if(UIScreenManager.Instance != null && UIScreenManager.Instance.CurrentScreen == this && Input.GetKeyDown(KeyCode.Mouse0)){
+
+			// If we are not casting a popup is not open
+			if(!this.isCastingPower && UIScreenManager.Instance.CurrentPopUp == null){
+
+				// TODO : if villager, or village selected, show popup info
+
+				// Show Popup powerList
+				this.OpenOtherPopUp("PowerList");
+
+			}
+			// If we are casting a power
+			else if(this.isCastingPower){
+
+				Vector3 castTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+				Debug.Log("UIScreenInGame.Update - Cast target : "+castTarget);
+
+				// TODO :
+				// this.CurrentlySelectedPower.CastAt(castTarget);
+
+			}
+
+
+		}
+
+	}
+
+
+
+
+}
