6e0c2fd1390c90a02d25a37cae4c1d17e10e9de9
Anton Monjon <monjon.an@gmail.com>
2016-01-31 07:57:11 +0100

    adding bubble prefab for icons to be displayed

---
+++ src/RitualMaker/Assets/Scripts/Icons/BubbleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleController : MonoBehaviour {
+
+	public GameObject Icon;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject go = (GameObject) GameObject.Instantiate (Icon);
+		go.transform.position = new Vector3 (go.transform.position.x, go.transform.position.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+//		mousePos.z = 0;
+//
+//		Debug.Log (mousePos);
+//
+//		GameObject go = (GameObject) GameObject.Instantiate (Icon, mousePos, Quaternion.identity);
+//		go.transform.position = mousePos;
+	}
+}
